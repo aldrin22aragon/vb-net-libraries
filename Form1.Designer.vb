6a7a348a8d6d2f50b62da8c0e7998380d6fa0cdc
Aldrin Aragon <aldrin22aragon@gmail.com>
2024-01-23 15:21:53 +0800

    Testing new librariesd

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NFaceView1 = New Neurotec.Biometrics.Gui.NFaceView()
         Me.ClockPanel1 = New vb_net_libraries.ClockPanel()
         Me.SuspendLayout()
         '
@@ -31,6 +32,20 @@ Partial Class Form1
         '
         Me.Timer1.Enabled = True
         Me.Timer1.Interval = 1000
+        '
+        'NFaceView1
+        '
+        Me.NFaceView1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.NFaceView1.Face = Nothing
+        Me.NFaceView1.FaceIds = Nothing
+        Me.NFaceView1.IcaoArrowsColor = System.Drawing.Color.Red
+        Me.NFaceView1.Location = New System.Drawing.Point(349, 111)
+        Me.NFaceView1.Name = "NFaceView1"
+        Me.NFaceView1.ShowIcaoArrows = True
+        Me.NFaceView1.ShowTokenImageRectangle = True
+        Me.NFaceView1.Size = New System.Drawing.Size(200, 162)
+        Me.NFaceView1.TabIndex = 1
+        Me.NFaceView1.TokenImageRectangleColor = System.Drawing.Color.White
         '
         'ClockPanel1
         '
@@ -44,6 +59,7 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.NFaceView1)
         Me.Controls.Add(Me.ClockPanel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -52,4 +68,5 @@ Partial Class Form1
     End Sub
     Friend WithEvents Timer1 As Timer
     Friend WithEvents ClockPanel1 As ClockPanel
+    Friend WithEvents NFaceView1 As Neurotec.Biometrics.Gui.NFaceView
 End Class
