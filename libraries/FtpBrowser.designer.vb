57da6c3c5dc7880b854023d898f73b582d0f9272
Aldrin Aragon <aldrin22aragon@gmail.com>
2024-01-24 13:30:48 +0800

    Add Ftp Browser design

---
+++ libraries/FtpBrowser.designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FtpBrowser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FtpBrowser))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnNext_ = New System.Windows.Forms.Button()
+        Me.BtnPrev_ = New System.Windows.Forms.Button()
+        Me.btnrefresh = New System.Windows.Forms.Button()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.lv = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(2, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(417, 44)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.AutoScroll = True
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Controls.Add(Me.btnNext_)
+        Me.Panel2.Controls.Add(Me.BtnPrev_)
+        Me.Panel2.Controls.Add(Me.btnrefresh)
+        Me.Panel2.Controls.Add(Me.btnSelect)
+        Me.Panel2.Controls.Add(Me.btnOpen)
+        Me.Panel2.Controls.Add(Me.lv)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(2, 46)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(417, 229)
+        Me.Panel2.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 195)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(31, 31)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'btnNext_
+        '
+        Me.btnNext_.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNext_.Location = New System.Drawing.Point(214, 198)
+        Me.btnNext_.Name = "btnNext_"
+        Me.btnNext_.Size = New System.Drawing.Size(30, 25)
+        Me.btnNext_.TabIndex = 7
+        Me.btnNext_.Text = "→"
+        Me.btnNext_.UseVisualStyleBackColor = True
+        '
+        'BtnPrev_
+        '
+        Me.BtnPrev_.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnPrev_.Location = New System.Drawing.Point(155, 198)
+        Me.BtnPrev_.Name = "BtnPrev_"
+        Me.BtnPrev_.Size = New System.Drawing.Size(30, 25)
+        Me.BtnPrev_.TabIndex = 6
+        Me.BtnPrev_.Text = "←"
+        Me.BtnPrev_.UseVisualStyleBackColor = True
+        '
+        'btnrefresh
+        '
+        Me.btnrefresh.BackgroundImage = CType(resources.GetObject("btnrefresh.BackgroundImage"), System.Drawing.Image)
+        Me.btnrefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnrefresh.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnrefresh.Location = New System.Drawing.Point(184, 198)
+        Me.btnrefresh.Name = "btnrefresh"
+        Me.btnrefresh.Size = New System.Drawing.Size(31, 25)
+        Me.btnrefresh.TabIndex = 5
+        Me.btnrefresh.UseVisualStyleBackColor = True
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSelect.Location = New System.Drawing.Point(308, 198)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(106, 25)
+        Me.btnSelect.TabIndex = 3
+        Me.btnSelect.Text = "Select Dir"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Enabled = False
+        Me.btnOpen.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOpen.Location = New System.Drawing.Point(249, 198)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(53, 25)
+        Me.btnOpen.TabIndex = 2
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'lv
+        '
+        Me.lv.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lv.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lv.FullRowSelect = True
+        Me.lv.GridLines = True
+        Me.lv.HideSelection = False
+        Me.lv.Location = New System.Drawing.Point(3, 2)
+        Me.lv.Name = "lv"
+        Me.lv.Size = New System.Drawing.Size(411, 192)
+        Me.lv.SmallImageList = Me.ImageList1
+        Me.lv.TabIndex = 0
+        Me.lv.UseCompatibleStateImageBehavior = False
+        Me.lv.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Name"
+        Me.ColumnHeader1.Width = 303
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Type"
+        Me.ColumnHeader2.Width = 103
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "document.png")
+        Me.ImageList1.Images.SetKeyName(1, "folder.png")
+        '
+        'FtpBrowser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(421, 277)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FtpBrowser"
+        Me.Padding = New System.Windows.Forms.Padding(2)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "FtpBrowser"
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents lv As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents btnSelect As System.Windows.Forms.Button
+    Friend WithEvents btnOpen As System.Windows.Forms.Button
+    Friend WithEvents btnrefresh As System.Windows.Forms.Button
+    Friend WithEvents btnNext_ As System.Windows.Forms.Button
+    Friend WithEvents BtnPrev_ As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+End Class
---
+++ libraries/FtpBrowser.vb
@@ -0,0 +1,323 @@
+﻿' please add WinSCP.exe startup Path 5.17.8.10803
+' add reference WinSCPnet.dll 5.17.8.0
+
+Public Class FtpBrowser
+   Dim sessionOptions As WinSCP.SessionOptions = Nothing
+   Dim lastPath As String = ""
+
+   Property DefPath As String = ""
+   Property mode As Mode_ = Mode_.none
+   Property multiSelect As Boolean = False
+   'Property hasSelected As Boolean = False
+
+   Private directoryWord As String = "Directory"
+   Private fileWord As String = "File"
+
+   Dim history As history_
+   Public ReturnString As String = ""
+   Public ReturnArray As String() = {}
+   Public ReturnFileInfoCollection As New Dictionary(Of String, WinSCP.RemoteFileInfo)
+
+   Friend WithEvents bg As System.ComponentModel.BackgroundWorker
+   Friend WithEvents tmr As Timer
+
+   Enum Mode_ As Integer
+      none = 0
+      file = 1
+      folder = 2
+   End Enum
+   Sub New(sessionOptions_ As WinSCP.SessionOptions, md As Mode_, Optional defPath As String = "/", Optional multipleSelection As Boolean = False)
+      ' This call is required by the designer.
+      InitializeComponent()
+      ' Add any initialization after the InitializeComponent() call.
+      Me.DefPath = defPath
+      Me.sessionOptions = sessionOptions_
+      mode = md
+      lastPath = ""
+      ReturnString = ""
+      ReturnArray = {}
+      history = New history_
+      Me.multiSelect = multipleSelection
+      Me.DialogResult = Windows.Forms.DialogResult.Cancel
+      bg = New System.ComponentModel.BackgroundWorker
+      bg.WorkerReportsProgress = True
+      tmr = New Timer
+   End Sub
+   Private Sub FtpBrowser_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+      If e.KeyCode = Keys.Escape Then
+         Me.Close()
+
+      End If
+   End Sub
+   Private Sub FtpBrowser_Load(sender As Object, e As EventArgs) Handles Me.Load
+      lv.MultiSelect = multiSelect
+      If mode = Mode_.folder Then
+         Me.Text = "Please select " & directoryWord
+         btnSelect.Text = String.Concat("Select ", directoryWord)
+      ElseIf mode = Mode_.file Then
+         Me.Text = "Please select " & fileWord
+         btnSelect.Text = String.Concat("Select ", fileWord)
+      End If
+   End Sub
+
+   Dim processing As Boolean = False
+
+   Function CreateLabel(name As String, text As String, path As String, loc As Point) As LinkLabel
+      Dim c As New LinkLabel
+      c.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+      c.Location = loc
+      c.Name = name
+      c.TabStop = True
+      c.Text = text
+      c.Visible = True
+      c.Tag = path
+      c.AutoSize = True
+      AddHandler c.LinkClicked, AddressOf linkClick
+      Panel1.Controls.Add(c)
+      Return c
+   End Function
+
+   Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnrefresh.Click
+      RunBackgroungWorker(lastPath)
+   End Sub
+   Private Sub FtpBrowser_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+      If sessionOptions IsNot Nothing Then
+         lastPath = DefPath
+         RunBackgroungWorker(DefPath)
+      Else
+         MsgBox("Please make sure session options is initialized", MsgBoxStyle.Critical)
+         Me.Close()
+      End If
+   End Sub
+   Sub linkClick(sender As Object, e As LinkLabelLinkClickedEventArgs)
+      Dim link As LinkLabel = CType(sender, LinkLabel)
+      If link.Tag IsNot Nothing Then
+         Dim pth As String = link.Tag
+         RunBackgroungWorker(pth)
+      End If
+   End Sub
+   Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+      If lv.SelectedItems.Count > 0 Then
+         If getStrings(Mode_.folder).Length > 0 Then
+            RunBackgroungWorker(getStrings(Mode_.folder)(0))
+         End If
+      End If
+   End Sub
+   Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+      If lv.SelectedItems.Count > 0 Then
+         If multiSelect Then
+            ReturnArray = getStrings(mode)
+         Else
+            ReturnString = getStrings(mode)(0)
+         End If
+         For Each i As ListViewItem In lv.SelectedItems
+            Dim type As String = i.SubItems(1).Text.ToString.ToUpper
+            If (mode = Mode_.folder And type = directoryWord.ToUpper) Or (mode = Mode_.file And type = fileWord.ToUpper) Then
+               Dim pth As String = lastPath.TrimEnd("/") & "/" & i.Text.ToString
+               ReturnFileInfoCollection.Add(pth, i.Tag)
+            End If
+         Next
+         Me.DialogResult = Windows.Forms.DialogResult.OK
+         Me.Close()
+      ElseIf mode = Mode_.folder Then
+         ReturnString = lastPath
+         Me.DialogResult = Windows.Forms.DialogResult.OK
+         Me.Close()
+      Else
+         MsgBox("Please select item" & IIf(Me.multiSelect, "s", ""), MsgBoxStyle.Critical)
+      End If
+   End Sub
+   Function getStrings(md As Mode_) As String()
+      Dim res As New List(Of String)
+      For Each i As ListViewItem In lv.SelectedItems
+         Dim type As String = i.SubItems(1).Text.ToString.ToUpper
+         Dim pth As String = lastPath.TrimEnd("/") & "/" & i.Text.ToString
+         If (md = Mode_.folder And type = directoryWord.ToUpper) Or (md = Mode_.file And type = fileWord.ToUpper) Then
+            res.Add(pth)
+         End If
+      Next
+      Return res.ToArray
+   End Function
+   Private Sub lv_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lv.SelectedIndexChanged
+      If lv.SelectedItems.Count > 0 Then
+         Dim itm As ListViewItem = lv.SelectedItems(0)
+         If itm.SubItems(1).Text.ToUpper = directoryWord.ToUpper Then
+            btnOpen.Enabled = True
+         Else
+            btnOpen.Enabled = False
+         End If
+         itm = itm
+      Else
+         btnOpen.Enabled = False
+      End If
+   End Sub
+
+   Private Sub lv_DoubleClick(sender As Object, e As EventArgs) Handles lv.DoubleClick
+      If btnOpen.Enabled Then
+         btnOpen.PerformClick()
+      End If
+   End Sub
+
+   Public Class history_
+      Dim curNdx As Integer = -1
+      Dim lst As New List(Of String)
+      Sub New()
+      End Sub
+      Function next_(Optional changeCurentHistory As Boolean = False) As String
+         Dim res As String = ""
+         Try
+            Dim newNdx As Integer = curNdx + 1
+            res = lst(newNdx)
+            If changeCurentHistory Then
+               curNdx = newNdx
+            End If
+         Catch ex As Exception
+            res = ""
+         End Try
+         Return res
+      End Function
+      Function Prev_(Optional changeCurentHistory As Boolean = False) As String
+         Dim res As String = ""
+         Try
+            Dim newNdx As Integer = curNdx - 1
+            res = lst(newNdx)
+            If changeCurentHistory Then
+               curNdx = newNdx
+            End If
+         Catch ex As Exception
+            res = ""
+         End Try
+         Return res
+      End Function
+      Sub addHistory(hist As String)
+         Dim newNdx As Integer = curNdx + 1
+         If curNdx = -1 Then
+            lst.Add(hist)
+            curNdx = 0
+         Else
+            Dim lastUlr As String = lst(curNdx)
+            If lastUlr <> hist Then
+               lst.Insert(newNdx, hist)
+               curNdx = newNdx
+            End If
+         End If
+      End Sub
+   End Class
+   Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnNext_.Click
+      If history.next_ <> Nothing Then
+         RunBackgroungWorker(history.next_(True))
+      End If
+   End Sub
+   Private Sub BtnPrev__Click(sender As Object, e As EventArgs) Handles BtnPrev_.Click
+      If history.Prev_ <> Nothing Then
+         RunBackgroungWorker(history.Prev_(True))
+      End If
+   End Sub
+   Dim bgRunThisPath As String = ""
+   Sub RunBackgroungWorker(pth As String)
+      If Not bg.IsBusy Then
+         tmr.Enabled = True
+         bgRunThisPath = pth
+         bg.RunWorkerAsync()
+      End If
+   End Sub
+   Private Sub bg_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bg.DoWork
+      tmr.Enabled = True
+      Dim path As String = bgRunThisPath
+      If Not processing Then
+         processing = True
+         Dim fls As WinSCP.RemoteFileInfoCollection = Nothing
+         Try
+            Dim ses As New WinSCP.Session()
+            ses.Open(sessionOptions)
+            Try
+               Dim directory As WinSCP.RemoteDirectoryInfo = ses.ListDirectory(path)
+               fls = directory.Files
+            Catch ex As Exception
+               MsgBox(("Please try again.§§" & ex.Message).Replace("§", vbNewLine), MsgBoxStyle.Critical, "Error on listing directory.")
+            End Try
+            ses.Dispose()
+            ses = Nothing
+         Catch ex As Exception
+            MsgBox(("Please try again.§§" & ex.Message).Replace("§", vbNewLine), MsgBoxStyle.Critical, "Error on connecting to ftp")
+         End Try
+         If fls IsNot Nothing Then
+            bg.ReportProgress(1, {path, fls})
+         End If
+         processing = False
+      End If
+   End Sub
+   Sub enables(bol As Boolean)
+      Panel1.Enabled = bol
+      lv.Enabled = bol
+      btnSelect.Enabled = bol
+      btnOpen.Enabled = bol
+   End Sub
+   Private Sub tmr_Tick(sender As Object, e As EventArgs) Handles tmr.Tick
+      If bg.IsBusy Then
+         If Not PictureBox1.Visible Then
+            PictureBox1.Visible = True
+            enables(False)
+         End If
+      Else
+         PictureBox1.Visible = False
+         enables(True)
+         tmr.Enabled = False
+      End If
+   End Sub
+   Private Sub bg_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles bg.ProgressChanged
+      Dim path As String = e.UserState(0).ToString
+      Dim fls As WinSCP.RemoteFileInfoCollection = e.UserState(1)
+      lastPath = path
+      lv.Items.Clear()
+      Dim fileInfo As WinSCP.RemoteFileInfo
+      Panel1.Controls.Clear()
+      For Each fileInfo In fls
+         If fileInfo.Name.Trim(".").Trim <> Nothing Then
+            If (mode = Mode_.folder And fileInfo.IsDirectory) Or mode = Mode_.file Then
+               Dim itm As ListViewItem = lv.Items.Add(fileInfo.Name)
+               If fileInfo.IsDirectory Then
+                  itm.SubItems.Add(directoryWord)
+                  itm.ImageIndex = 1
+               Else
+                  itm.SubItems.Add(fileWord)
+                  itm.ImageIndex = 0
+               End If
+               itm.Tag = fileInfo
+               itm.EnsureVisible()
+            End If
+         End If
+      Next
+      Dim tmpName As String = ""
+      Dim left_ As Integer = 5
+      Dim top_ As Integer = 2
+      Dim labelCounts As Integer = 1
+      Dim pth As String = ""
+      For i As Integer = 0 To path.Length - 1
+         Dim vl As String = path(i)
+         tmpName = tmpName & vl
+         pth = pth & vl
+         If vl = "/" Or i = path.Length - 1 Then
+            Dim nm As String = "MyLabel" & labelCounts
+            Dim loc As New Point(left_, top_)
+            Dim lbl As LinkLabel = CreateLabel(nm, tmpName, pth, loc)
+            left_ = left_ + lbl.Width
+            labelCounts += 1
+            tmpName = ""
+         End If
+      Next
+      history.addHistory(path)
+      If history.next_ <> Nothing Then
+         btnNext_.Enabled = True
+      Else
+         btnNext_.Enabled = False
+      End If
+      If history.Prev_ <> Nothing Then
+         BtnPrev_.Enabled = True
+      Else
+         BtnPrev_.Enabled = False
+      End If
+      lv_SelectedIndexChanged(New Object, New EventArgs)
+   End Sub
+
+End Class
