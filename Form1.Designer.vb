22ff6de9be6d50e7f13775db8eb5fab6caf660f8
Aldrin Aragon <aldrin22aragon@gmail.com>
2024-01-25 18:19:32 +0800

    _

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,7 +24,6 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Timer1
@@ -32,26 +31,15 @@ Partial Class Form1
         Me.Timer1.Enabled = True
         Me.Timer1.Interval = 1000
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(372, 81)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Timer1 As Timer
-    Friend WithEvents Button1 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -7,7 +7,7 @@
    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
    End Sub
 
-   Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+   Private Sub Button1_Click(sender As Object, e As EventArgs) 
 
    End Sub
 End Class
