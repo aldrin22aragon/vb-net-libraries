9682bc3c60f7df00383173d934c5b58f4c787225
Aldrin Aragon <aldrin22aragon@gmail.com>
2024-02-01 03:52:33 +0800

    Upgrade

--- libraries/AOA_Timer.vb
+++ libraries/AOA_Timer.vb
@@ -4,14 +4,13 @@ Public Class AOA_Timer
    Event Tick(e As TickEventInfo)
    Public maximumSeconds As Integer
    Dim SW As New Stopwatch
-   ReadOnly mode As Mode_enum
-   Enum Mode_enum As Integer
-      RunOnce = 1
-      PabalikBalik = 2
-   End Enum
-   Sub New(seconds As Integer, Optional _mode As Mode_enum = Mode_enum.RunOnce)
+   'ReadOnly mode As Mode_enum
+   'Enum Mode_enum As Integer
+   '   RunOnce = 1
+   '   PabalikBalik = 2
+   'End Enum
+   Sub New(seconds As Integer)
       maximumSeconds = seconds
-      mode = _mode
    End Sub
    Sub StartTimer(Optional overWriteSeconds As Integer = 0)
       If overWriteSeconds > 0 Then maximumSeconds = overWriteSeconds
@@ -26,28 +25,32 @@ Public Class AOA_Timer
       Timer.Start()
    End Sub
    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
-      RaiseEvent Tick(New TickEventInfo() With {.IsTimeReached = IsTimeReached(), .Span = RemainingTime()})
       If IsTimeReached() Then
-         If mode = Mode_enum.RunOnce Then
-            SW.Stop()
-            Timer.Stop()
-         Else
-            RestartTimer()
-         End If
+         SW.Stop()
+         Timer.Stop()
       End If
+      RaiseEvent Tick(New TickEventInfo() With {
+                      .IsTimeReached = IsTimeReached(),
+                      .Span = RemainingTime(),
+                      .secondsRemaining = RemainingSeconds()
+      })
    End Sub
 
    Public Function IsTimeReached() As Boolean
       Return SW.Elapsed.Seconds >= maximumSeconds
    End Function
-   Public Function RemainingTime() As TimeSpan
+   Function RemainingTime() As TimeSpan
       Dim res As Integer = maximumSeconds - SW.Elapsed.Seconds
       Dim span As TimeSpan = TimeSpan.FromSeconds(res)
       Return span
    End Function
+   Function RemainingSeconds() As Integer
+      Return maximumSeconds - SW.Elapsed.Seconds
+   End Function
 
    Class TickEventInfo
       Public IsTimeReached As Boolean
+      Public secondsRemaining As Integer
       Public Span As TimeSpan
    End Class
 End Class
