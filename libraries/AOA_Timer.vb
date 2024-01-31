c647588c45f9df6626c6673318fc7b769636f399
Aldrin Aragon <aldrin22aragon@gmail.com>
2024-02-01 02:02:04 +0800

    Add span

--- libraries/AOA_Timer.vb
+++ libraries/AOA_Timer.vb
@@ -26,7 +26,7 @@ Public Class AOA_Timer
       Timer.Start()
    End Sub
    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
-      RaiseEvent Tick(New TickEventInfo() With {.IsTimeReached = IsTimeReached(), .RemainingSeconds = RemainingSeconds()})
+      RaiseEvent Tick(New TickEventInfo() With {.IsTimeReached = IsTimeReached(), .Span = RemainingTime()})
       If IsTimeReached() Then
          If mode = Mode_enum.RunOnce Then
             SW.Stop()
@@ -40,12 +40,14 @@ Public Class AOA_Timer
    Public Function IsTimeReached() As Boolean
       Return SW.Elapsed.Seconds >= maximumSeconds
    End Function
-   Public Function RemainingSeconds() As Integer
-      Return maximumSeconds - SW.Elapsed.Seconds
+   Public Function RemainingTime() As TimeSpan
+      Dim res As Integer = maximumSeconds - SW.Elapsed.Seconds
+      Dim span As TimeSpan = TimeSpan.FromSeconds(res)
+      Return span
    End Function
 
    Class TickEventInfo
       Public IsTimeReached As Boolean
-      Public RemainingSeconds As Integer
+      Public Span As TimeSpan
    End Class
 End Class
