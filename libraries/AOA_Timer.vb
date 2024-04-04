8491a33c27e2469815715c1532e9d2b1a9d7fa56
Aldrin Aragon <aldrin22aragon@gmail.com>
2024-04-04 20:28:13 +0800

    224-4-04

--- libraries/AOA_Timer.vb
+++ libraries/AOA_Timer.vb
@@ -3,7 +3,7 @@ Public Class AOA_Timer
    Friend WithEvents Timer As New Timer
    Event Tick(e As TickEventInfo)
    Public maximumSeconds As Integer
-   Dim SW As New Stopwatch
+   Dim SW As Stopwatch = Nothing
    'ReadOnly mode As Mode_enum
    'Enum Mode_enum As Integer
    '   RunOnce = 1
@@ -12,17 +12,30 @@ Public Class AOA_Timer
    Sub New(seconds As Integer)
       maximumSeconds = seconds
    End Sub
-   Sub StartTimer(Optional overWriteSeconds As Integer = 0)
-      If overWriteSeconds > 0 Then maximumSeconds = overWriteSeconds
-      SW = New Stopwatch
-      SW.Start()
-      Timer.Start()
+
+   Sub StartRunning(Optional overWriteSeconds As Integer = 0)
+      If SW Is Nothing Then
+         If overWriteSeconds > 0 Then maximumSeconds = overWriteSeconds
+         SW = New Stopwatch
+         SW.Start()
+         Timer.Start()
+      Else
+         If SW.IsRunning Then
+            Throw New Exception("Timer is already running")
+         Else
+            RestartTimer()
+         End If
+      End If
    End Sub
    Sub RestartTimer(Optional overWriteSeconds As Integer = 0)
-      If overWriteSeconds > 0 Then maximumSeconds = overWriteSeconds
-      SW = New Stopwatch
-      SW.Start()
-      Timer.Start()
+      If SW IsNot Nothing Then
+         If overWriteSeconds > 0 Then maximumSeconds = overWriteSeconds
+         SW = New Stopwatch
+         SW.Start()
+         Timer.Start()
+      Else
+         Throw New Exception("Timer must be start first")
+      End If
    End Sub
    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
       If IsTimeReached() Then
